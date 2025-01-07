d80180a6c8ecd997329fcdc91f9052471323f0b5
Firdavs <firdavs@Firdavss-MacBook-Air.local>
2025-01-08 00:58:12 +0900

    Complete all the registering and other pages. Also finish the styling. Some minor things might be changed later

--- index.js
+++ index.js
@@ -19,12 +19,18 @@ app.use(
         secret: process.env.SESSION_SECRET,
         resave: false,
         saveUninitialized: true,
+        cookie: { 
+            maxAge: null 
+        },
     })
 );
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const db = new pg.Client({
     user: process.env.PG_USER,
@@ -36,19 +42,42 @@ const db = new pg.Client({
 
 db.connect();
 
+let sort_by = "n.id ASC";
 let currentPostId;
-let sort_by = "id ASC";
 
 async function gettingThePost() {
-    const response = await db.query("SELECT * FROM notes WHERE id = $1", [currentPostId]);
+    console.log("Fetching post with ID:", currentPostId);
+
+    const response = await db.query(
+        `SELECT n.id, n.title, n.isbn, n.cover_id, n.published_year, n.rating, n.review, u.nickname, n.user_id, u.email, u.nickname 
+        FROM notes n
+        JOIN users u ON u.id = n.user_id
+        WHERE n.id = $1
+        ORDER BY ${sort_by}`, [currentPostId]);
 
     const specificPost = response.rows[0];
+
+    console.log("Fetched post data:", specificPost);
+
     return specificPost;
+}
+
+// Getting logged user's nickname from passport
+async function gettingTheCurrentUser(req) {
+    const nickname = req.user.nickname;
+    const result = await db.query("SELECT id FROM users WHERE nickname = $1", [nickname]);
+    const userId = result.rows[0].id;
+
+    return userId;
 }
 
 app.get("/", async (req, res) => {
     try {
-        const response = await db.query(`SELECT * FROM notes ORDER BY ${sort_by}`);
+        const response = await db.query(
+            `SELECT n.id, n.title, n.isbn, n.cover_id, n.published_year, n.rating, n.review, u.nickname, n.user_id, u.email, u.nickname 
+            FROM notes n
+            JOIN users u ON u.id = n.user_id
+            ORDER BY ${sort_by}`);
         const postsList = response.rows;
         
         res.render("index.ejs", {
@@ -62,17 +91,35 @@ app.get("/", async (req, res) => {
     }
 });
 
-app.get("/review/:id", async (req, res) => {
-    try {
-        currentPostId = parseInt(req.params.id, 10);
-        const currentPost = await gettingThePost();
+// login and sign-up endpoints
 
-        res.render("reviewPage.ejs", {
-            post: currentPost
-        });
-    } catch (err) {
-        console.log("The request was failed. ", err.message);
-    } 
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
+
+app.get("/signUp", (req, res) => {
+    res.render("signUp.ejs");
+});
+
+//
+
+app.get("/review/:id", async (req, res) => {
+    if (req.isAuthenticated()) {
+        try {
+            currentPostId = parseInt(req.params.id, 10);
+            const currentPost = await gettingThePost();
+            const currentUser = await gettingTheCurrentUser(req);
+
+            res.render("reviewPage.ejs", {
+                post: currentPost,
+                user: currentUser,
+            });
+        } catch (err) {
+            console.log("The request was failed. ", err.message);
+        } 
+    } else {
+        res.redirect("/login");
+    }
 });
 
 app.get("/edit/:id", async (req, res) => {
@@ -85,15 +132,18 @@ app.get("/edit/:id", async (req, res) => {
         });
     } catch (err) {
         console.log("The request was failed. ", err.message);
-    }
+    }    
 });
 
 app.get("/create", (req, res) => {
-    try {
-        res.render("postCreation.ejs", {
-        });
-    } catch (err) {
-        console.log("The request was failed. ", err.message);
+    if (req.isAuthenticated()) {
+        try {
+            res.render("postCreation.ejs");
+        } catch (err) {
+            console.log("The request was failed. ", err.message);
+        }
+    } else {
+        res.redirect("/login");
     }
 });
 
@@ -128,8 +178,12 @@ app.post("/create", async (req, res) => {
         const published_year = req.body.published_year;
         const rating = req.body.rating;
         const review = req.body.review;
+        
+        const user_id = await gettingTheCurrentUser(req);
 
-        await db.query("INSERT INTO notes (title, isbn, cover_id, published_year, rating, review) VALUES ($1, $2, $3, $4, $5, $6)", [title, isbn, cover_id, published_year, rating, review]);
+        await db.query("INSERT INTO notes (title, isbn, cover_id, published_year, rating, review, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7)", 
+            [title, isbn, cover_id, published_year, rating, review, user_id]
+        );
         
         res.redirect("/");
     } catch (err) {
@@ -143,11 +197,15 @@ app.post("/update/:id", async (req, res) => {
         const newRating = req.body.rating;
         const newReview = req.body.review;
 
-        await db.query("UPDATE notes SET rating = $1, review = $2 WHERE id = $3", [newRating, newReview, currentPostId]);
+        await db.query("UPDATE notes SET rating = $1, review = $2 WHERE id = $3", 
+            [newRating, newReview, currentPostId]
+        );
 
         const updatedPost = await gettingThePost();
+        const currentUser = await gettingTheCurrentUser(req);
         res.render("reviewPage.ejs", {
-            post: updatedPost
+            post: updatedPost,
+            user: currentUser,
         });
     } catch (err) {
         console.log("The request was failed. ", err.message);
@@ -155,20 +213,27 @@ app.post("/update/:id", async (req, res) => {
 });
 
 app.post("/delete/:id", async (req, res) => {
-    try {
-        const idToDelete = parseInt(req.params.id, 10);
+    if (req.isAuthenticated()) {
+        try {
+            const idToDelete = parseInt(req.params.id, 10);
 
-        await db.query("DELETE FROM notes WHERE id = $1", [idToDelete]);
-        res.redirect("/");
-    } catch (err) {
-        console.log("The request was failed. ", err.message);
+            await db.query("DELETE FROM notes WHERE id = $1", [idToDelete]);
+            res.redirect("/");
+        } catch (err) {
+            console.log("The request was failed. ", err.message);
+        }    
+    } else {
+        res.render("/login");
     }
+    
 });
 
 app.get("/sort/:id", (req, res) => {
     try {
         const sort = req.params.id;
         if (sort == "title") {
+            sort_by = sort + " ASC";
+        } else if (sort == "u.nickname") {
             sort_by = sort + " ASC";
         } else {
             sort_by = sort + " DESC";
@@ -180,37 +245,119 @@ app.get("/sort/:id", (req, res) => {
     }
 });
 
+// special sort for myPosts page
+app.get("/sortPost/:id", (req, res) => {
+    try {
+        const sort = req.params.id;
+        if (sort == "title") {
+            sort_by = sort + " ASC";
+        } else if (sort == "u.nickname") {
+            sort_by = sort + " ASC";
+        } else {
+            sort_by = sort + " DESC";
+        }
+        
+        res.redirect("/myPosts");
+    } catch (err) {
+        console.log("The request was failed. ", err.message);
+    }
+});
+
+app.get("/myPosts", async (req, res) => {
+    if (req.isAuthenticated()) {
+        try {
+            const user_id = await gettingTheCurrentUser(req);
+            const result = await db.query(
+            `SELECT n.id, n.title, n.isbn, n.cover_id, n.published_year, n.rating, n.review, u.nickname, n.user_id, u.email, u.nickname 
+            FROM notes n
+            JOIN users u ON u.id = n.user_id
+            WHERE user_id = $1
+            ORDER BY ${sort_by}`, [user_id]);
+            const posts = result.rows;
+
+            res.render("myPosts.ejs", {
+                posts: posts,
+                user: user_id,
+            });
+        } catch (err) {
+            console.log("The request was failed. ", err.message);
+        }    
+    } else {
+        res.redirect("/login");
+    }
+    
+});
+
 // NEW ENDPOINTS
-app.get("/myPosts", (req, res) => {
-    try {
-        
-    } catch (err) {
-        console.log("The request was failed. ", err.message);
-    }
-});
 
-app.get("/signUp", (req, res) => {
-    try {
-        const nickname = req.body.name;
-        const email = req.body.email;
-        const password = req.body.password;
+app.get("/auth/google",
+    passport.authenticate("google", {
+        scope: ["profile", "email"],
+    })
+);
+
+app.get("/auth/google/posts",
+    passport.authenticate("google", {
+        successRedirect: "/",
+        failureRedirect: "/login",
+    })
+);
+
+app.post("/login",
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/login",
+    }), (req, res) => {
+        // additional logic here to handle the rememberMe functionality
         const rememberTheUser = req.body.rememberTheUser;
 
+        if (rememberTheUser) {
+            req.session.cookie.maxAge = 1000 * 60 * 60 *24;
+        } else {
+            req.session.cookie.expires = false;
+        }
 
-
-    } catch (err) {
-        console.log("The request was failed. ", err.message);
+        res.redirect("/");
     }
-});
+);
 
-app.get("/login", (req, res) => {
+app.post("/signUp", async  (req, res) => {
+    const nickname = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const rememberTheUser = req.body.rememberTheUser;
+
     try {
-        const email = req.body.email;
-        const password = req.body.password;
-        const rememberTheUser = req.body.rememberTheUser;
+        const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+            email,
+        ]);
 
-        
+        if (checkResult.rows.length > 0) {
+            res.redirect("/login");
+        } else {
+            bcrypt.hash(password, saltRounds, async (err, hash) => {
+                if (err) {
+                    console.error("Error hashing the password: ", err);
+                } else {
+                    const result = await db.query("INSERT INTO users (email, password, nickname) VALUES ($1, $2, $3) RETURNING *", [
+                        email, hash, nickname,
+                    ]);
+                    const user = result.rows[0];
 
+                    // additional logic here to handle the rememberMe functionality
+                    if (rememberTheUser) {
+                        req.session.cookie.maxAge = 1000 * 60 * 60 * 24;
+                    } else {
+                        req.session.cookie.expires = false;
+                    }
+
+                    req.login(user, (err) => {
+                        console.log("Successfully registered!");
+                        res.redirect("/");
+                    });
+                }
+            });
+        }    
     } catch (err) {
         console.log("The request was failed. ", err.message);
     }
@@ -254,7 +401,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:3000/auth/google/secrets",
+            callbackURL: "http://localhost:3000/auth/google/posts",
             userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
         },
         async (accessToken, refreshToken, profile, cb) => {
@@ -264,8 +411,8 @@ passport.use(
                     profile.email,
                 ]);
                 if (result.rows.length === 0) {
-                    const newUser = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [
-                        profile.email, "google"
+                    const newUser = await db.query("INSERT INTO users (email, password, nickname) VALUES ($1, $2, $3)", [
+                        profile.email, "google", profile.given_name,
                     ]);
                     return cb(null, newUser.rows[0]);
                 } else {
--- queries.sql
+++ queries.sql
@@ -1,9 +1,26 @@
--- Create a database if not exists
-CREATE DATABASE IF NOT EXISTS BookNotes;
+-- Create the database if you don't have it. If you have just delete this line of command
+CREATE DATABASE BookNotes;
 
--- This one if you already have a table in this name or just want to rewrite the table
+
+-- This one is for: if you already have a table in this name or just want to rewrite the table
+DROP TABLE IF EXISTS users;
+
+--Creating users table
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(30),
+    password VARCHAR(100),
+    nickname VARCHAR(20)
+);
+
+-- Inserting random users
+INSERT INTO users (email, password, nickname)
+VALUES 
+    ('test1@gmail.com', 'nobodycares1', 'Who is that'),
+    ('test2@gmail.com', 'nobodycares2', 'Killabab');
+
+-- This one is for: if you already have a table in this name or just want to rewrite the table
 DROP TABLE IF EXISTS notes;
-
 
 -- Creating a table
 CREATE TABLE notes (
@@ -13,20 +30,20 @@ CREATE TABLE notes (
     cover_id VARCHAR(15),
     published_year VARCHAR(15),
     rating INTEGER,
-    review TEXT
+    review TEXT,
+    user_id INTEGER REFERENCES users(id)
 );
 
 -- Inserting values
-INSERT INTO notes (title, isbn, cover_id, published_year, rating, review)
-VALUES ('Rich Dad, Poor Dad', '9784480863300', '8315603', '1990', '8', 'Nulla auctor quam quis orci consequat, ac tempor sapien pharetra. Morbi facilisis lacinia ornare. Sed magna turpis, elementum vel nulla id, hendrerit tincidunt leo. Suspendisse finibus sem pharetra orci sagittis congue. Curabitur augue justo, cursus at ipsum et, elementum aliquet erat. Duis congue malesuada sem, at bibendum eros porta in. Morbi eu blandit dui. Vestibulum suscipit consequat ante, nec varius erat mattis sit amet.
+INSERT INTO notes (title, isbn, cover_id, published_year, rating, review, user_id)
+VALUES 
+    ('Rich Dad, Poor Dad', '9784480863300', '8315603', '1990', '8', 'Nulla auctor quam quis orci consequat, ac tempor sapien pharetra. Morbi facilisis lacinia ornare. Sed magna turpis, elementum vel nulla id, hendrerit tincidunt leo. Suspendisse finibus sem pharetra orci sagittis congue. Curabitur augue justo, cursus at ipsum et, elementum aliquet erat. Duis congue malesuada sem, at bibendum eros porta in. Morbi eu blandit dui. Vestibulum suscipit consequat ante, nec varius erat mattis sit amet.
 Donec accumsan, elit ut scelerisque ultricies, leo tellus imperdiet mauris, in vestibulum leo enim sit amet augue. Nullam posuere neque vestibulum nunc pellentesque, ut consequat quam sagittis. Sed metus diam, fringilla sit amet libero nec, tempor tristique leo. Suspendisse enim ex, pulvinar non sapien quis, pharetra pretium est. Donec semper mauris ut convallis facilisis. Phasellus eu blandit eros. In hac habitasse platea dictumst.
 Maecenas tempus arcu ut nulla rhoncus, ut venenatis lectus mattis. Sed molestie tincidunt nunc at malesuada. Nam sagittis leo dui, at laoreet lorem aliquam porttitor. Curabitur ultricies accumsan nulla, vel porta tortor consectetur nec. Nulla imperdiet felis diam, tempor efficitur erat interdum consequat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac 
-turpis egestas. Donec consectetur mi eget libero pharetra scelerisque volutpat cursus turpis. Vivamus sed sapien neque.'), 
-		('Sickness and Health', '9780749313173', '2585871', '1984', '9', 'Nulla ut turpis ipsum. Duis tempor blandit est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean congue nisl est, eget vestibulum ipsum ultricies vitae. Curabitur ullamcorper, lorem at vestibulum efficitur, ipsum odio malesuada eros, nec dapibus augue massa mollis odio. Integer id scelerisque risus. Integer iaculis nibh eget enim porttitor, et lacinia elit bibendum. Morbi aliquet arcu in elit molestie, vel varius velit elementum. Maecenas molestie pretium euismod. Maecenas vel dui sit amet est accumsan maximus. Phasellus volutpat purus massa, eget varius mi iaculis sit amet. Ut mattis et arcu eget consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lobortis massa ac fermentum ornare. Maecenas odio dolor, fermentum 
+turpis egestas. Donec consectetur mi eget libero pharetra scelerisque volutpat cursus turpis. Vivamus sed sapien neque.', 1), 
+	('Sickness and Health', '9780749313173', '2585871', '1984', '9', 'Nulla ut turpis ipsum. Duis tempor blandit est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean congue nisl est, eget vestibulum ipsum ultricies vitae. Curabitur ullamcorper, lorem at vestibulum efficitur, ipsum odio malesuada eros, nec dapibus augue massa mollis odio. Integer id scelerisque risus. Integer iaculis nibh eget enim porttitor, et lacinia elit bibendum. Morbi aliquet arcu in elit molestie, vel varius velit elementum. Maecenas molestie pretium euismod. Maecenas vel dui sit amet est accumsan maximus. Phasellus volutpat purus massa, eget varius mi iaculis sit amet. Ut mattis et arcu eget consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lobortis massa ac fermentum ornare. Maecenas odio dolor, fermentum 
 vel rutrum vitae, eleifend eget ex. Maecenas lectus risus, mollis non lorem eget, venenatis feugiat ante.
 Nam aliquet, ligula eu luctus pretium, est velit ullamcorper eros, vitae molestie magna ex in mi. Nam vitae libero non turpis fringilla rhoncus quis in massa. Suspendisse ultricies justo et elit gravida, et feugiat tortor convallis. Proin maximus gravida nunc, 
-sed vulputate ante porta et. Nam facilisis lacus urna, id mattis ligula hendrerit id. Curabitur vitae dictum diam. Morbi sit amet purus a eros fermentum mattis ullamcorper id orci. Quisque ultrices tempor metus. Donec at ante euismod, auctor felis sed, molestie nisi. Ut faucibus mattis tempus. Integer a lorem eleifend erat dapibus mattis. Nullam pharetra, justo quis venenatis mollis, erat neque ultrices magna, vel imperdiet lacus erat a tellus.');
-
-
+sed vulputate ante porta et. Nam facilisis lacus urna, id mattis ligula hendrerit id. Curabitur vitae dictum diam. Morbi sit amet purus a eros fermentum mattis ullamcorper id orci. Quisque ultrices tempor metus. Donec at ante euismod, auctor felis sed, molestie nisi. Ut faucibus mattis tempus. Integer a lorem eleifend erat dapibus mattis. Nullam pharetra, justo quis venenatis mollis, erat neque ultrices magna, vel imperdiet lacus erat a tellus.', 2);
 
 
